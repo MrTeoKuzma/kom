0ca38916c484bfbe1235b60a0be381cee6296b6f
shtayerc <david.murko5@gmail.com>
2019-01-10 13:05:16 +0100

    Projectile animation

--- projectile.cpp
+++ projectile.cpp
@@ -4,7 +4,7 @@
 #include "map.h"
 #include "window.h"
 
-Projectile::Projectile(double radius, double x, double y, double mX, double mY, double speed): radius(radius), x(x), y(y), pX(x), pY(y), speed(speed){
+Projectile::Projectile(double radius, double x, double y, double mX, double mY, double speed, string type): radius(radius), x(x), y(y), pX(x), pY(y), speed(speed), type(type){
     
     if(mX > x){
         k =  (mY - y) / (mX - x);
@@ -19,13 +19,17 @@ Projectile::Projectile(double radius, double x, double y, double mX, double mY, 
     if(x == mX){
         stepX = 0;
         k = y > mX ? -1 : 1;
-    }   
+    }
+    loadImg();
 }
 
+
 void Projectile::draw(sf::RenderWindow * window){
-    sf::CircleShape circle(radius);
-    circle.setPosition(x, y);
-    window->draw(circle);
+    sf::Sprite tmp(*sprite);
+    tmp.setPosition(x, y);
+    if(animation)
+        animation->setSprite(tmp);
+    window->draw(tmp);
 }
 
 void Projectile::action(Map *map){
@@ -34,4 +38,10 @@ void Projectile::action(Map *map){
     
     if(x < 0 || x > WINWIDTH || y < 0 || y > WINHEIGHT)
         map->removeProjectile(this);
+}
+
+void Projectile::loadImg(){
+    sprite = new sf::Texture();
+    sprite->loadFromFile("img/skill/"+type);
+    animation = new Animation(200);
 }
--- projectile.h
+++ projectile.h
@@ -2,6 +2,8 @@
 #define PROJECTILE_H_
 #include <SFML/Graphics.hpp>
 #include "map.h"
+#include "animation.h"
+
 class Map;
 class Projectile{
     private:
@@ -13,11 +15,15 @@ class Projectile{
         double pX;
         double pY;
         double speed;
+        string type;
+        sf::Texture *sprite;
+        Animation * animation;
 
     public:
-        Projectile(double radius, double x, double y, double mX, double mY, double speed);
+        Projectile(double radius, double x, double y, double mX, double mY, double speed, string type);
         void draw(sf::RenderWindow * window);
         void action(Map *map);
+        void loadImg();
 };
 
 #endif
